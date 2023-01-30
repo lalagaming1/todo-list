1efb7357765d6da4bf9549eb23572dd069c50610
lalagaming1 <123898156+lalagaming1@users.noreply.github.com>
2023-01-30 19:09:12 +0200

    finished UI
    Ui is complete but there isnt functionality yet

--- main.py
+++ main.py
@@ -7,7 +7,7 @@ app.width = 400
 app.set_resizable = False
 #Creating Widgets
 New_Task_lbl = gp.Label(app, "New Task")
-New_Task_lsb = gp.Listbox(app)
+New_Task_lsb = gp.Input(app)
 Tasks_lbl = gp.Label(app, "Tasks")
 Tasks_lsb = gp.Listbox(app)
 Delete_Task_btn = gp.Button(app, "Delete Task", None)
@@ -17,4 +17,7 @@ app.set_grid(5,3)
 app.add(New_Task_lbl, 1, 1)
 app.add(New_Task_lsb, 1, 2)
 app.add(Tasks_lbl, 2, 1)
+app.add(Tasks_lsb, 2, 2)
+app.add(Clear_All_btn, 3, 1)
+app.add(Delete_Task_btn, 3, 2)
 app.run()

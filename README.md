28f1e224e9016e647fdbac4d481523b36f6e8f31
lalagaming1 <123898156+lalagaming1@users.noreply.github.com>
2023-01-30 21:34:33 +0200

    Finished adding functionality
    finished adding functionality and changing the name of the contexts of the application and finishing the app

--- README.md
+++ README.md
@@ -1,2 +1,5 @@
 # todo-list
 a todo list app written in the language python using the GooeyPie library.
+
+# Notes
+Note: The app doesnt save tasks(yet....)
--- main.py
+++ main.py
@@ -1,33 +1,73 @@
-#Importing GooeyPie Module
+#Importing GooeyPie module
 import gooeypie as gp
-#Defining Functions
-def Delete_Task():
-    Tasks_lsb.selected = Tasks_lsb.selected - Tasks_lsb.items
 
+#Defining functions
+def add_new_task(event):
+    """Adds a new task from when the Enter/Return key is pressed"""
+    if event.key['name'] == 'Return' and new_task_inp.text != "":
+        todo_lst.add_item(new_task_inp.text)
+        new_task_inp.clear()
+        new_task_inp.focus()
 
-def clear_all():
-    Tasks_lsb.items = []
+def move_task(event):
+    """Moves a task from one listbox to another"""
+    if event.widget == todo_lst:
+        # move the task from the todo list to the done list
+        done_lst.add_item(todo_lst.remove_selected())
+    else:
+        # move the task back from the done list to the todo list
+        todo_lst.add_item(done_lst.remove_selected())
 
+def delete_task(event):
+    """Deletes a task from the todo list"""
+    todo_lst.remove_selected()
+    new_task_inp.focus()
 
+def all_done(event):
+    """Moves all tasks from the todo list to the done list"""
+    done_lst.items += todo_lst.items
+    todo_lst.items = []
 
-#Initializing GooeyPie and window properties(width, resizabilty, etc..)
-app = gp.GooeyPieApp("Todo-list")
+def clear_all(event):
+    """Remove all tasks from the done list"""
+    done_lst.items = []
+
+#Initializing GooeyPie
+app = gp.GooeyPieApp('Todo list')
 app.width = 400
-app.set_resizable = False
-#Creating Widgets(labels, listboxes, input, etc..)
-New_Task_lbl = gp.Label(app, "New Task")
-New_Task_lsb = gp.Input(app)
-Tasks_lbl = gp.Label(app, "Tasks")
-Tasks_lsb = gp.Listbox(app)
-Delete_Task_btn = gp.Button(app, "Delete Task", Delete_Task)
-Clear_All_btn = gp.Button(app, "Clear All", clear_all)
-#Setting grid and Placing widgets on window(Setting Grid is used to place widgets on window)
-app.set_grid(5,3)
-app.add(New_Task_lbl, 1, 1)
-app.add(New_Task_lsb, 1, 2)
-app.add(Tasks_lbl, 2, 1)
-app.add(Tasks_lsb, 2, 2)
-app.add(Clear_All_btn, 3, 1)
-app.add(Delete_Task_btn, 3, 2)
-#Running Application
+#Making the app not resizable
+app.set_resizable(False)
+
+# Creating widgets(label, listboxes, input, etc..)
+new_task_lbl = gp.Label(app, 'New task')
+new_task_inp = gp.Input(app)
+todo_lbl = gp.Label(app, 'Todo list')
+todo_lst = gp.Listbox(app)
+delete_task_btn = gp.Button(app, 'Delete task', delete_task)
+all_done_btn = gp.Button(app, 'All done!', all_done)
+done_lbl = gp.Label(app, 'Done!')
+done_lst = gp.Listbox(app)
+clear_all_btn = gp.Button(app, 'Clear all', clear_all)
+
+# Setting up Event Listeners(that lisen for mouse clicks, keyboard presses, etc..)
+new_task_inp.add_event_listener('key_press', add_new_task)
+todo_lst.add_event_listener('double_click', move_task)
+done_lst.add_event_listener('double_click', move_task)
+
+# Add widgets to window and setting grid which is used for placing widgets on window
+app.set_grid(5, 3)
+app.set_column_weights(0, 1, 1)
+app.set_row_weights(0, 1, 0, 1, 0)
+app.add(new_task_lbl, 1, 1, align='right')
+app.add(new_task_inp, 1, 2, column_span=2, fill=True)
+app.add(todo_lbl, 2, 1, align='right')
+app.add(todo_lst, 2, 2, column_span=2, fill=True, stretch=True)
+app.add(delete_task_btn, 3, 2, fill=True)
+app.add(all_done_btn, 3, 3, fill=True)
+app.add(done_lbl, 4, 1, align='right')
+app.add(done_lst, 4, 2, column_span=2, fill=True, stretch=True)
+app.add(clear_all_btn, 5, 3, fill=True)
+#Focusing on the new task input box to quickly add tasks to the todo list
+new_task_inp.focus()
+#Running the app
 app.run()
